3e85a15c9f299fb6dc7272fed19242ad15ed4bfc
Loks665 <111459885+Loks665@users.noreply.github.com>
2022-10-23 16:48:18 +0300

    Update nick.lua

--- nick.lua
+++ nick.lua
@@ -1,1 +1,1 @@
-NiXsis eusuntpula123 mila08 vanyasmall XZT_08 ggvvps Nesstazzy SDDOdw Bruh621 sashabez1011 _DxD_ Loks sayn donttouchthis 
+sqemmy NiXsis eusuntpula123 mila08 vanyasmall XZT_08 ggvvps Nesstazzy SDDOdw Bruh621 sashabez1011 _DxD_ Loks sayn donttouchthis 
